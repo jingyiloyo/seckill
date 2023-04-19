89eabd0f3a7e978324aa78555f33d2768bacabaa
jingyiloyo <2125411156@qq.com>
2023-04-19 20:01:54 +0800

    Initial commit

---
+++ src/main/java/com/xxxx/seckill/conroller/GoodsController.java
@@ -0,0 +1,93 @@
+package com.xxxx.seckill.conroller;
+
+import com.xxxx.seckill.pojo.User;
+import com.xxxx.seckill.service.IGoodsService;
+import com.xxxx.seckill.vo.DetailVo;
+import com.xxxx.seckill.vo.GoodsVo;
+import com.xxxx.seckill.vo.RespBean;
+import io.netty.util.internal.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Controller
+@RequestMapping("/goods")
+public class GoodsController {
+
+    @Autowired
+    private IGoodsService goodsService;
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Autowired
+    private ThymeleafViewResolver thymeleafViewResolver;
+
+    //跳转到商品详情页面
+    @RequestMapping(value = "/toList",produces = "text/html;charset=UTF-8")
+    @ResponseBody
+    public String toList(Model model, User user, HttpServletRequest request, HttpServletResponse response){
+        //从redis中获取界面，如果不为空，直接返回界面
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        String html = (String)valueOperations.get("goodsList");
+        if(StringUtils.hasLength(html)){
+            return html;
+        }
+        model.addAttribute("user", user);
+        model.addAttribute("goodsList", goodsService.findGoodsVo());
+
+        //如果redis中页面为空，则需要生成页面的字符串存入redis中
+        WebContext context = new WebContext(request,response,request.getServletContext(),request.getLocale(),model.asMap());
+        //process("goodsList",context);goodsList代表的是页面，会找goodsList.html，将context中的数据加载到goodsList.html中，生成html字符串。
+        html = thymeleafViewResolver.getTemplateEngine().process("goodsList",context);
+        if(!StringUtils.isEmpty(html)){
+            valueOperations.set("goodsList",html,1, TimeUnit.HOURS);
+        }
+        return html;
+    }
+
+    @RequestMapping("/detail/{goodsId}")
+    @ResponseBody
+    public RespBean toDetail(User user,@PathVariable Long goodsId){
+        GoodsVo goodsVo = goodsService.findGoodsVoByGoodsId(goodsId);
+        Date startDate = goodsVo.getStartDate();
+        Date endDate = goodsVo.getEndDate();
+        Date nowDate = new Date();
+        //秒杀状态 0 代表未开始 1 进行中 2 结束
+        int seckillStatus = 0;
+        //秒杀倒计时
+        int remainSeconds = 0;
+        //判断当前状态
+        if (nowDate.before(startDate)) {
+            //当前时间在开始时间之前，代表秒杀还未开始
+            //获取距离秒杀的时间差
+            remainSeconds = (int) ((startDate.getTime()-nowDate.getTime())/1000);
+        } else if (nowDate.after(endDate)) {
+            //结束状态
+            seckillStatus = 2;
+            remainSeconds = -1;
+        } else {
+            //进行中
+            seckillStatus = 1;
+            remainSeconds = 0;
+        }
+        DetailVo detailVo = new DetailVo();
+        detailVo.setUser(user);
+        detailVo.setGoodsVo(goodsVo);
+        detailVo.setSeckillStatus(seckillStatus);
+        detailVo.setRemainSeconds(remainSeconds);
+        return RespBean.success(detailVo);
+    }
+}
---
+++ src/main/java/com/xxxx/seckill/mapper/SeckillGoodsMapper.java
@@ -0,0 +1,19 @@
+package com.xxxx.seckill.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xxxx.seckill.pojo.SeckillGoods;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ *
+ *
+ *
+ * @author chen
+ *
+ */
+public interface SeckillGoodsMapper extends BaseMapper<SeckillGoods> {
+
+}
